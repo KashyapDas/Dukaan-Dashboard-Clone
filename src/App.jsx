3a70ff81abc8ad946e9c2c61d625e4ca69a832d1
Kashyap Das <kashyap.education2234@gmail.com>
2024-11-09 23:18:28 +0530

    Creating the sidebar features for the small devices

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import Navbar from "./components/Navbar";
+import { useRecoilState } from "recoil";
+import { NavAtoms } from "./atoms/NavAtoms";
 
 function App() {
+
+  const [navAtoms, setNavAtoms] = useRecoilState(NavAtoms);
+
+
   return (
-    <div className="w-screen h-screen bg-red-400 sm:bg-green-500 overflow-hidden p-1">
+    <div className="w-screen h-screen sm:bg-green-500 overflow-hidden relative">
       
-      <div className="flex">
+      <div className="flex mt-1 h-fit items-center">
         {/* Hamburg Menu */}
         <div className="sidebar h-fit w-fit">
-          <GiHamburgerMenu className="text-3xl cursor-pointer " />
+          <GiHamburgerMenu className="text-3xl cursor-pointer" onClick={()=>setNavAtoms(prev=>!prev)} />
         </div>
         {/* Search Bar will appear here */}
         <div className="searchBar m-auto">
-          <input type="search" placeholder="Search Features, tutorials, etc." className="p-1 rounded-md" />
+          <input type="search" placeholder="Search Features, tutorials, etc." className="p-1 rounded-md border-2 border-solid border-black" />
         </div>
       </div>
 
+      <div 
+        className={`transition-all duration-300 ease-in-out absolute -translate-y-[0.6%] ${
+          navAtoms 
+            ? 'opacity-100 translate-x-0' 
+            : 'opacity-0 -translate-x-full pointer-events-none'
+        } h-screen`}
+      >
+        <Navbar />
+      </div>
 
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import { FaShopware } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+import { useSetRecoilState } from "recoil";
+import { NavAtoms } from "../atoms/NavAtoms";
 
-function Navbar() {
+const Navbar = React.memo(() =>{
+  const setNavAtoms = useSetRecoilState(NavAtoms);
+  console.log("Navbar Rendered...");
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="h-screen w-[50vw] absolute -translate-y-[5%] bg-slate-800 text-white p-1">
+      {/* Brand Logo and remove button will be present here */}
+      <div className="topSideBar w-full flex justify-between items-center">
+        <h1 className="w-fit h-fit flex justify-center items-center">
+          <FaShopware className="text-3xl" />
+          <span className="ml-2 text-2xl font-semibold ">Dukaan</span>
+        </h1>
+        <ImCross
+          className="text-xl cursor-pointer"
+          onClick={() => setNavAtoms((prev) => !prev)}
+        />
+      </div>
 
-export default Navbar
+      <hr className="w-full bg-slate-300 opacity-80 mt-3 mb-3" />
+
+      <ul>
+            <li>Dashboard</li>
+            <li>Orders</li>
+            <li>Delivery</li>
+            <li>Products</li>
+            <li>Analytics</li>
+            <li>Discounts</li>
+            <li>Audiences</li>
+            <li>Appereances</li>
+            <li>Plugins</li>
+            <li>Settings</li>
+            
+            
+      </ul>
+
+    </div>
+  );
+})
+
+export default Navbar;
